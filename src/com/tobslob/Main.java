2b9dfdefdd6a28965a92d9ed08c0350b90e0c4ea
tobslob <kazmobileapp@gmail.com>
2021-08-03 13:27:45 +0100

    fix: not need to save file data to memory

--- src/com/tobslob/Main.java
+++ src/com/tobslob/Main.java
@@ -1,5 +1,6 @@
 package com.tobslob;
 
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.io.*;
 
@@ -12,23 +13,23 @@ public class Main {
             FileInputStream fis = new FileInputStream(text);
 
             byte[] data = new byte[(int) text.length()];
-            fis.read(data);
-            fis.close();
-
-            String str = new String(data, "UTF-8");
 
             HashMap<String, Integer> map = new HashMap<>();
 
-            String[] splitStr = str.replaceAll("[\";,.:()]", "").replaceAll("[\n/]", " ").toLowerCase().split(" ");
-            for (String s : splitStr) {
-                if (map.containsKey(s)) {
-                    map.put(s, map.get(s) + 1);
-                } else {
-                    map.put(s, 1);
+            while(fis.read(data) != -1) {
+                String str = new String(data, StandardCharsets.UTF_8);
+                String[] splitStr = str.replaceAll("[\";,.:()]", "").replaceAll("[\n/]", " ").toLowerCase().split(" ");
+                for (String s : splitStr) {
+                    if (map.containsKey(s)) {
+                        map.put(s, map.get(s) + 1);
+                    } else {
+                        map.put(s, 1);
+                    }
                 }
+                System.out.println(map);
             }
 
-            System.out.println(map);
+            fis.close();
         } catch (IOException io){
             System.out.println("File not found.");
         } catch (Exception e) {
